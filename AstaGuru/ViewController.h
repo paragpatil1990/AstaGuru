1f7c07f10958299ee6767550000f3db69f760a3c
Sumit Mashalkar <sumitmashalkar@gmail.com>
2016-08-29 14:02:26 +0530

    Initial Commit

---
+++ AstaGuru/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AstaGuru
+//
+//  Created by Aarya Tech on 29/08/16.
+//  Copyright © 2016 Aarya Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AstaGuru/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AstaGuru
+//
+//  Created by Aarya Tech on 29/08/16.
+//  Copyright © 2016 Aarya Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
